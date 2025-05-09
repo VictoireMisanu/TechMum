21772125cdbfae6d57189dc1be60ec188fccca34
Victoire <vicmisanu63@gmail.com>
2025-05-09 14:19:51 +0100

    agriculture module added

--- src/components/pages/agriculture.tsx
+++ src/components/pages/agriculture.tsx
@@ -3,32 +3,32 @@ import Header from '../navBar'
 
 
 
-export default function Couture () {
+export default function Agriculture () {
     return(
         <div className="">
             <Header/>
             <div className='w-full h-20 flex justify-center items-center bg-heroPattern bg-cover bg-no-repeat bg-center'>
-                <h1 className='font-playfair font-extrabold text-4xl text-white'>Informatique</h1>
+                <h1 className='font-playfair font-extrabold text-4xl text-white'>Agriculture et élévage</h1>
             </div>
             <div id='filter' className='w-full h-20 flex justify-center items-center gap-40 font-bold text-2xl bg-primaryPink/40'>
                 <button className='hover:bg-primaryPink px-3 py-1 rounded-md'>Cours texte</button>
                 <button className='hover:bg-primaryPink px-3 py-1 rounded-md'>Cours vidéo</button>
             </div>
             <div id='courses' className='w-full h-auto flex flex-col items-center justify-center gap-10 p-10'>
-                <section id='digitalLitteracy' className='w-full h-auto flex flex-col justify-center gap-5'>
-                    <h1 className='font-roboto font-extrabold text-2xl text-black'>Bureautique</h1>
+                <section id='agriculture' className='w-full h-auto flex flex-col justify-center gap-5'>
+                    <h1 className='font-roboto font-extrabold text-2xl text-black'>Cultures</h1>
                     <div className='w-full h-auto flex items-center justify-center gap-5'>
-                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Word' description='Apprenez à utiliser Word' imageUrl='/images/communityManagement.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
-                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Excel' description='Apprenez à utiliser Excel' imageUrl='/images/communityManagement.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
-                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='PowerPoint' description='Apprenez à utiliser PowerPoint' imageUrl='/images/communityManagement.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
+                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Sols et fertilité' description='Apprenez à utiliser Word' imageUrl='/images/agriculture.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
+                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Epices' description='Apprenez à utiliser Excel' imageUrl='/images/agriculture.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
+                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Céréales' description='Apprenez à utiliser PowerPoint' imageUrl='/images/agriculture.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
                     </div>
                 </section>
-                <section id='advanced' className='w-full h-auto flex flex-col justify-center gap-5'>
-                    <h1 className='font-roboto font-extrabold text-2xl text-black'>Informatique avancée</h1>
+                <section id='breeding' className='w-full h-auto flex flex-col justify-center gap-5'>
+                    <h1 className='font-roboto font-extrabold text-2xl text-black'>Elévage</h1>
                     <div className='w-full h-auto flex items-center justify-center gap-5'>
-                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Marketing Design' description='Devenez spécialiste marketing digital' imageUrl='/images/communityManagement.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
-                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Community Management' description='Cours complet en community manager' imageUrl='/images/communityManagement.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
-                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Graphic Design' description='Devenez graphic designer' imageUrl='/images/communityManagement.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
+                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Bovins' description='Devenez spécialiste marketing digital' imageUrl='/images/womanWithChicken.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
+                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Volaille' description="Apprendre l'élévage de la volaille" imageUrl='/images/woman&chicken.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
+                        <CourseCard className="w-1/3 h-full flex flex-col gap-2 justify-start bg-slate-100 shadow-sm shadow-slate-500 hover:shadow-black hover:bg-slate-300" title='Pâturages' description='Apprendre à' imageUrl='/images/womanWithChicken.jpg' courses={7} time={3} test={1} price={50} language='Français'path='/course3'/>
                     </div>
                 </section>
             
--- src/components/pages/couture.tsx
+++ src/components/pages/couture.tsx
@@ -6,7 +6,7 @@ export default function Couture () {
         <div className="">
             <Header/>
             <div className='w-full h-20 flex justify-center items-center bg-heroPattern bg-cover bg-no-repeat bg-center'>
-                <h1 className='font-playfair font-extrabold text-4xl text-white'>Informatique</h1>
+                <h1 className='font-playfair font-extrabold text-4xl text-white'>Couture</h1>
             </div>
             <div id='filter' className='w-full h-20 flex justify-center items-center gap-40 font-bold text-2xl bg-primaryPink/40'>
                 <button className='hover:bg-primaryPink px-3 py-1 rounded-md'>Cours texte</button>
