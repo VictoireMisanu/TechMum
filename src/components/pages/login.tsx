102cf2d9b4acaea0580fa0d623411f19093f770c
Victoire <vicmisanu63@gmail.com>
2025-05-15 14:24:42 +0100

    feat-login functionality added

--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,16 +1,18 @@
 
 import type React from "react"
 
-import { useState } from "react"
-import {Link} from "react-router-dom"
+import { useEffect, useState } from "react"
+import {Link, useNavigate} from "react-router-dom"
+import { authenticateUser } from "../../services/api";
 
-export default function RegistrationForm() {
+export default function Connexion() {
+  
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
-    prenom: "",
-    nom: "",
     email: "",
-    motDePasse: "",
-  })
+    password: "",
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
@@ -20,12 +22,33 @@ export default function RegistrationForm() {
     }))
   }
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    console.log("Données du formulaire soumises:", formData)
-    // Logique pour envoyer les données à votre API
-  }
+    useEffect(() => {
+        const token = localStorage.getItem('auth_token');
+        if (token) {
+            navigate('/courses');
+        }
+    }, [navigate]);
 
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+
+        const formData = new FormData(e.currentTarget)
+        formData.forEach((value, key) => {
+        console.log(key, value);
+        });
+        
+
+        await authenticateUser(formData).then((result)=>{
+
+            localStorage.setItem('auth_token', result.token.token);
+            localStorage.setItem('user_info', JSON.stringify(result.userInfo));
+            console.log(result)
+            alert("vous êtes connecté")
+            navigate('/courses')
+        }).catch((error)=>{
+            alert(error.response.data.message + " ; " +error.response.data.error)
+        })
+    }
   return (
     <div className="w-full flex flex-col items-center gap-6 justify-center ">
       <div id="header" className="px-10 py-5 flex items-center h-16 w-full border-b border-gray-100 shadow-sm shadow-mainPink">
@@ -49,9 +72,9 @@ export default function RegistrationForm() {
 
           <input
             type="password"
-            name="motDePasse"
+            name="password"
             placeholder="Mot de passe"
-            value={formData.motDePasse}
+            value={formData.password}
             onChange={handleChange}
             className="w-full border border-gray-300 p-3 focus:outline-none focus:ring-0 focus:border-gray-400"
             required
